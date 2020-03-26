7c24bad3387a3d4d164ae66119ef55c88f9cd848
Ryan.Wang <ranwang1995@outlook.com>
2020-03-27 02:48:38 +0800

    [WangRan] feat: add constructor and getter methods

--- src/main/java/com/thoughtworks/entities/User.java
+++ src/main/java/com/thoughtworks/entities/User.java
@@ -6,6 +6,11 @@ public class User {
     private String phone;
     private String email;
     private String password;
+    private int failedLoginCount;
+    private boolean locked;
+
+    public User() {
+    }
 
     public User(String username, String phone, String email, String password) {
         this.username = username;
@@ -14,17 +19,16 @@ public class User {
         this.password = password;
     }
 
-    public User(String id, String username, String phone, String email, String password) {
+    public User(String id, String username, String phone, String email, String password, int failedLoginCount, boolean locked) {
         this.id = id;
         this.username = username;
         this.phone = phone;
         this.email = email;
         this.password = password;
+        this.failedLoginCount = failedLoginCount;
+        this.locked = locked;
     }
 
-    public String getId() {
-        return id;
-    }
 
     public String getUsername() {
         return username;
@@ -41,4 +45,18 @@ public class User {
     public String getPassword() {
         return password;
     }
+
+    public int getFailedLoginCount() {
+        return failedLoginCount;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
 }
